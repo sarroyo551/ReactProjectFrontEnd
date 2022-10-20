fb7db57c94441cb1fe9fd8653961a96ae093b12d
Santi Arroyo <santi.arroyo551@gmail.com>
2022-10-20 11:20:21 -0400

    adds form to new athlete page

--- src/screens/NewAthlete.js
+++ src/screens/NewAthlete.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function NewAthlete() {
   return (
-    <div className='content'>NewAthlete</div>
+    <div className='content'>
+      <h1>Create Your Own Fighter</h1>
+      <form className='athlete-form'>
+        <label>Name  <input /> </label>
+        <label>Image  <input /> </label>
+        <label>Record  <input /> </label>
+        <label>Reach  <input /> </label>
+        <label>Country of Origin  <input /> </label>
+        <label>Weight <input /> </label>
+        <label>Age  <input /> </label>
+        <label>Height  <input /> </label>
+       
+      </form>
+    </div>
   )
 }
 
