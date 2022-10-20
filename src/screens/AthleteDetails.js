c43e64cd562d5261d05abeafe1f6e65a099f1a83
Santi Arroyo <santi.arroyo551@gmail.com>
2022-10-20 11:13:35 -0400

    adds more css to the athlete details

--- src/screens/AthleteDetails.js
+++ src/screens/AthleteDetails.js
@@ -1,38 +1,43 @@
-import {useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function AthleteDetails() {
-  const params = useParams()
+  const params = useParams();
   // console.log(params)
-  const [fighter, setFighter] = useState(null)
+  const [fighter, setFighter] = useState(null);
 
   useEffect(() => {
     fetch(`http://localhost:3000/Athletes/${params.id}`)
       .then((r) => r.json())
       .then((data) => {
-        setFighter(data)
-      })
-  },[])
-
-
+        setFighter(data);
+      });
+  }, []);
 
   return (
-    <div className='content'>
+    <div className="content">
       {fighter && (
         <>
-        <h1>{fighter.name}</h1>
-        <img src={fighter.image} alt={fighter.name}/>
-        <h3>Weight: {fighter.weight}</h3>
-        <p>Reccord: {fighter.record}</p>
-        <p>Reach: {fighter.reach}</p>
-        <p>Country of Origin: {fighter['country of origin']}</p>
-        <p>Age: {fighter.age}</p>
-        <p>Height: {fighter.height}</p>
+          <h1>{fighter.name}</h1>
+          <div className="details-page">
+            <img
+              src={fighter.image}
+              alt={fighter.name}
+              className="athlete-image"
+            />
+            <div className='athlete-details'>
+              <h3>Weight: {fighter.weight}</h3>
+              <p>Reccord: {fighter.record}</p>
+              <p>Reach: {fighter.reach}</p>
+              <p>Country of Origin: {fighter["country of origin"]}</p>
+              <p>Age: {fighter.age}</p>
+              <p>Height: {fighter.height}</p>
+            </div>
+          </div>
         </>
-      )}      
-      </div>
-  )
+      )}
+    </div>
+  );
 }
 
-export default AthleteDetails
+export default AthleteDetails;
