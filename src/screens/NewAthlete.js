bd1bc27d6ada986e903615519c736c3f57102059
Santi Arroyo <santi.arroyo551@gmail.com>
2022-10-25 10:55:27 -0400

    adds state to new athlete page

--- src/screens/NewAthlete.js
+++ src/screens/NewAthlete.js
@@ -1,41 +1,57 @@
-import React from 'react'
+import {useState} from 'react'
 
 function NewAthlete() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    image: '',
+    record: '',
+    reach: '',
+    country: '',
+    weight: '',
+    age: '',
+    height: ''
+  })
+
+  function handleChange (e) {
+    console.log(e.target.name)
+  } 
+
   return (
     <div className='content'>
       <h1 className='new-athlete-heading'>Create Your Own Fighter</h1>
       <form className='athlete-form'>
         <div className='form-details'>
           <label>Name </label> 
-          <input />
+          <input name='name' onChange={handleChange}/>
         </div>
         <div className='form-details'>
           <label>Image </label> 
-          <input />
+          <input name='image' onChange={handleChange}/>
         </div>
         <div className='form-details'>
           <label>Record </label> 
-          <input />
+          <input name='record' onChange={handleChange}/>
         </div>
         <div className='form-details'>
           <label>Reach </label> 
-          <input />
+          <input name='reach' onChange={handleChange}/>
         </div>
         <div className='form-details'>
           <label>Country of Origin </label> 
-          <input />
+          <input name='country' onChange={handleChange}/>
         </div>
         <div className='form-details'>
           <label>Weight </label> 
-          <input />
+          <input name='weight' onChange={handleChange}/>
         </div>
         <div className='form-details'>
           <label>Age </label> 
-          <input />
+          <input name='age' onChange={handleChange}/>
         </div>
         <div className='form-details'>
           <label>Height </label> 
-          <input />
+          <input name='height' onChange={handleChange}/>
         </div>
         <button type='submit' className='submit-button'>Submit Fighter</button>
       </form>
